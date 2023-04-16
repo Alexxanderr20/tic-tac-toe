2d1e91074d47bfcdf7a680d9ca8513be0f7d48af
Edward Munoz <alexmu419@icloud.com>
2023-04-16 00:35:24 -0500

    Add rows and columns

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const board = ["X", "X", "X", "O", "O", "O", "X", "X", "X"];
-const game_board_container = document.getElementById("board-container");
+const game_board_container = document.getElementById("boardContainer");
 const board_cells = document.getElementsByClassName(".block"); 
 
 
+function renderingBoard(board-container)
+{
+for (let i = 0; i < boardContainer.length; i++) 
+    {
+        const row = document.createElement("div");
+      for(let j = 0; j < boardContainer[i].length; j++)
+      {
+        const col = document.createElement("span");
+      }
+      col.textContent = boardContainer[i][j];
 
+      
+        // cell.classList.add("block");
+        // cell.textContent = board[i];
+       //game_board_container.appendChild(cell);
+    }
+}
 // const gameboard = (() => {
 //   const board = ["X", "X", "X", "O", "O", "O", "X", "X", "X"];
 //   const getBoard = () => board;
