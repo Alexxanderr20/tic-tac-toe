7a7b9753d5c847998dee084af6f17db393c9b990
Edward Munoz <alexmu419@icloud.com>
2023-04-20 16:03:28 -0500

    Added files via upload

--- script.js
+++ script.js
@@ -1,43 +1,26 @@
-const board = ["X", "X", "X", "O", "O", "O", "X", "X", "X"];
-const game_board_container = document.getElementById("boardContainer");
-const board_cells = document.getElementsByClassName(".block"); 
 
-
-function renderingBoard(board-container)
+function renderBoard()
 {
-for (let i = 0; i < boardContainer.length; i++) 
-    {
-        const row = document.createElement("div");
-      for(let j = 0; j < boardContainer[i].length; j++)
-      {
-        const col = document.createElement("span");
-      }
-      col.textContent = boardContainer[i][j];
+  let winner = false;
+  let draw = false;
+  let loser = false;
 
-      
-        // cell.classList.add("block");
-        // cell.textContent = board[i];
-       //game_board_container.appendChild(cell);
-    }
-}
-// const gameboard = (() => {
-//   const board = ["X", "X", "X", "O", "O", "O", "X", "X", "X"];
-//   const getBoard = () => board;
-//   const setBoard = (index, player) => {
-//     board[index] = player;
-//   };
-//   return { getBoard, setBoard };
-// });
+  const humanPlayer = "X";
+  const computerPlayer = "O";
+  const gameBoard = document.querySelectorAll('.spot');
 
-// const displayController = (() => {
-//     const board = gameboard.getBoard();
-//     const cells = document.querySelectorAll(".cell");
-//     const displayBoard = () => {
-//         cells.forEach((cell, index) => {
-//         cell.textContent = board[index];
-//         });
-//     };
-//     return { displayBoard };
-// });
+  var board = [
+  ["","",""],
+  ["","",""],
+  ["","",""]
+];
 
-// const renderBoard = () => { 
+ }
+
+ function startGame()
+ {
+  spot.forEach(() => {
+    spot.innerHTML = '';
+    spot.addEventListener('click', handleClick.bind(null,spot, index));
+  });
+ }
